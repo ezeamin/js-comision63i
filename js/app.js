e7822a32c352de092438ca556917253bc479702c
Ezequiel Amin <ezequielamin@outlook.com>
2023-07-18 15:25:45 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let edad1;
+edad1 = prompt('Ingrese su edad');
+
+let edad2;
+edad2 = prompt('Ingrese la otra edad');
+
+let edad1Num = Number(edad1);
+let edad2Num = Number(edad2);
+
+console.log(edad1Num + edad2Num);
+
+if (edad1Num > edad2Num) {
+  console.log('La mayor edad es la 1');
+} else if (edad1Num < edad2Num) {
+  console.log('La mayor edad es la 2');
+} else {
+  console.log('Las edades son iguales');
+}
---
+++ js/switch.js
@@ -0,0 +1,58 @@
+/*
+2- Escribir un programa que solicite una nota (número) de 0 a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+
+Si ingresó un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+
+Ejemplo:
+
+Input: 5 -> Output: Suficiente
+Input: 50 -> Output: Número erróneo
+Input: hola10 -> Output: Introduce un número válido
+*/
+
+// PASOS ---------------------------------------------------
+
+/*
+1- Pedimos el numero
+2- Verificar si es un numero
+3- Verificar que esté entre 0 y 10------------
+4- Analizar los rangos y el numero ingresado
+5- Mostrar la calificacion en un alert
+*/
+
+// PRIMERA VERSION ------------------------------------------
+
+// let numero = prompt("Ingrese un numero del 0 al 10")
+let numero = -9;
+
+if (isNaN(numero)) {
+  // Si NO es un numero, ingresa al if
+  console.log("No ingresó un número valido.");
+} else if (!(numero >= 0 && numero <= 10)) {
+  // ES un numero, pero fuera del rango
+  console.log("Ingrese un numero dentro del rango establecido");
+} else {
+  // Rango 0-2
+  if (numero >= 0 && numero <= 2) {
+    console.log("Muy deficiente");
+  } else if (numero <= 4) {
+    console.log("Insuficiente");
+  } else if (numero <= 6) {
+    console.log("Suficiente");
+  } else if (numero === 7) {
+    console.log("Bien");
+  } else if (numero <= 9) {
+    console.log("Notable");
+  } else {
+    console.log("Sobresaliente");
+  }
+}
+
+// SEGUNDA VERSION ------------------------------------------
