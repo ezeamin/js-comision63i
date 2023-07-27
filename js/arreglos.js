95c1012218f456ccb0fa83f15e0bfe1ec9f1f4bc
Ezequiel Amin <ezequielamin@outlook.com>
2023-07-27 16:30:33 -0300

    agrego texto de arrays

--- js/arreglos.js
+++ js/arreglos.js
@@ -115,3 +115,32 @@ console.log(emojis.length);
 // });
 
 // console.log(alumnosFiltrados);
+
+
+/*
+Crear un arreglo
+Acceder a elementos del arreglo: []
+Longitud del arreglo: length
+Modificar elementos del arreglo: []
+Agregar elementos al final del arreglo: push
+Eliminar elementos del final del arreglo: pop
+Agregar elementos al inicio del arreglo: unshift
+Eliminar elementos del inicio del arreglo: shift
+Encontrar el índice de un elemento en el arreglo: indexOf
+Eliminar elementos mediante el índice: splice
+Recortar arreglo: slice
+Encontrar si existe un elemento en el arreglo: includes
+Unir elementos en un string: join
+Unir dos arreglos: concat
+Invertir un arreglo: reverse
+
+---------------------------
+Callbacks
+---------------------------
+
+filter
+find
+sort
+forEach
+...
+*/
